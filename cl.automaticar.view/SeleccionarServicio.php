0e1e7d4c9bf97c3f69729d6f591233413718ada1
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-23 13:05:44 -0400

    a

--- cl.automaticar.view/SeleccionarServicio.php
+++ cl.automaticar.view/SeleccionarServicio.php
@@ -6,10 +6,47 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <title>AutomatiCar</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+
         <title></title>
     </head>
     <body>
+        <div class="content">
+                                                                    <h2 class="align-center">Agendar Hora</h2>
+								<hr />
+								<form action="#" method="post">
+									<div class="field half first">
+										<label for="name">Número de patente</label>
+										<input name="name" id="name" type="text" placeholder="Patente">
+									</div>
+									<div class="field half">
+										<label for="email">E-mail</label>
+										<input name="email" id="email" type="email" placeholder="Email">
+									</div>
+									<div class="field">
+										<label for="dept">Department</label>
+										<div class="select-wrapper">
+											<select name="dept" id="dept">
+												<option value="">- linea 170 -</option>
+												<option value="1">linea 171</option>
+												<option value="1">linea 172</option>
+												<option value="1">linea 173</option>
+												<option value="1">linea 174</option>
+											</select>
+										</div>
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+									</div>
+									<ul class="actions align-center">
+										<li><input value="Send Message" class="button special" type="submit"></li>
+									</ul>
+								</form>
+							</div>
         <?php
         // put your code here
         ?>
