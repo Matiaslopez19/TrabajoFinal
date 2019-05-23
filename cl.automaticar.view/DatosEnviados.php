0be63559ce4fc792925e42a3ad1037de2efa49a4
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-23 14:21:59 -0400

    Datos

--- cl.automaticar.view/DatosEnviados.php
+++ cl.automaticar.view/DatosEnviados.php
@@ -6,10 +6,43 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <title>AutomatiCar</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+
         <title></title>
     </head>
     <body>
+        <div class="content">
+
+                                                                    
+                                                                    <h2 class="align-center">Cambiar contraseña</h2>
+								<hr />
+								<form action="#" method="post">
+		
+									<div class="field half">
+										<label>Código</label>
+										<input name="Email" id="email" type="email" placeholder="">
+									</div>
+                                                                        <div class="field half">
+										<label>Nueva contraseña</label>
+										<input name="Email" id="email" type="email" placeholder="">
+									</div>
+	
+                                                                    <br>
+									
+                                                                        <ul class="actions align-center">
+										<li><input value="Cambiar contraseña" class="button special" type="submit"><a</li>
+									</ul>
+									<ul class="actions align-center">
+                                                                                <li><input value="Volver" class="button special" type="submit"><a</li>
+                                                                            
+                                                                          
+									</ul>
+                                                                        
+								</form>
+							</div>
         <?php
         // put your code here
         ?>
