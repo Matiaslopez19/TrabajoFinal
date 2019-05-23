4505d23b521c8f03b8fce7a10adbdd3f0000199a
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-23 15:47:56 -0400

    Historial

--- cl.automaticar.view/Historial.php
+++ cl.automaticar.view/Historial.php
@@ -6,10 +6,35 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <title>AutomatiCar</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+
         <title></title>
     </head>
     <body>
+        <div class="content">
+
+                                                                    
+                                                                    <h2 class="align-center">Historial</h2>
+								<hr />
+								<form action="#" method="post">
+									<div class="field half">
+										<label>Ingrese patente del vehículo</label>
+										<input name="patente" id="email" type="email" placeholder="AA0000 - AAAA0000">
+									</div>
+                                                                    <br>
+									
+                                                                        <ul class="actions align-center">
+										<li><input value="Buscar y descargar" class="button special" type="submit"></li>
+									</ul>
+									<ul class="actions align-center">
+										<li><input value="Atrás" class="button special" type="submit"></li>
+									</ul>
+                                                                        
+								</form>
+							</div>
         <?php
         // put your code here
         ?>
