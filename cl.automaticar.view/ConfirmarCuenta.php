fe7b1305d78fc93682bf8a88916d62a507e6aeac
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-27 12:36:59 -0400

    ConfirmarCuenta

--- cl.automaticar.view/ConfirmarCuenta.php
+++ cl.automaticar.view/ConfirmarCuenta.php
@@ -6,10 +6,34 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <title>AutomatiCar</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+
         <title></title>
     </head>
     <body>
+        <div class="content">
+
+                                                                    
+                                                                    <h2 class="align-center">Confirmar cuenta</h2>
+								<hr />
+								<form action="#" method="post">
+									<div class="field">
+                                                                            <div class="field half">
+										<label>Código de confirmación</label>
+                                                                                <input name="codigo" id="codigo" type="text" placeholder="Código">
+                                                                            </div>  
+                                                                            <div class="field half">
+                                                                                <a href="">Reenviar Código</a>
+                                                                            </div>                                                                             
+									<ul class="actions align-center">
+										<li><input value="Aceptar" class="button special" type="submit"></li>
+									</ul>
+                                                                        
+								</form>
+							</div>
         <?php
         // put your code here
         ?>
