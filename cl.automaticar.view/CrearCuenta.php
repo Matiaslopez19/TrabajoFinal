739c95a521732e0d32d19087b27444d4de1a2f47
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-27 21:43:37 -0400

    seleccionarcrear

--- cl.automaticar.view/CrearCuenta.php
+++ cl.automaticar.view/CrearCuenta.php
@@ -6,11 +6,54 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="../assets/css/main.css" />
+        <title>AutomatiCar</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+
         <title></title>
     </head>
     <body>
+        <div class="content">
+
+                                                                    
+                                                                    <h2 class="align-center">Crear cuenta</h2>
+								<hr />
+								<form action="#" method="post">
+									<div class="field">
+                                                                            <div class="field half">
+										<label>Nombre</label>
+										<input name="Nombre" id="nombre" type="text" placeholder="Nombre">
+                                                                            </div>
+                                                                            <div class="field half">
+										<label>Apellido Paterno</label>
+										<input name="apellidoP" id="apellidoP" type="text" placeholder="Apellido Paterno">
+                                                                            </div>
+                                                                            <div class="field half">
+										<label>Apellido Materno</label>
+										<input name="apellidoM" id="apellidoM" type="text" placeholder="Apellido Materno">
+                                                                            </div>
+                                                                            <div class="field half">
+										<label>Email</label>
+                                                                                <input name="email" id="email" type="email" placeholder="Email">
+                                                                            </div>
+                                                                            <div class="field half">
+										<label>Contraseña</label>
+                                                                                <input name="contraseña" id="contraseña" type="password" placeholder="Contraseña">
+                                                                            </div>
+                                                                            <div class="field half">
+										<label>Confirmar contraseña</label>
+                                                                                <input name="contraseña1" id="contraseña1" type="password" placeholder="Confirmar contraseña">
+                                                                            </div>									
+                                                                        <ul class="actions align-center">
+										<li><input type="button" class="button special" value="Atrás" onClick=" window.location.href='Login.php' "></li> 
+									</ul>
+									<ul class="actions align-center">
+										<li><input value="Crear cuenta" class="button special" type="submit"></li>
+									</ul>
+                                                                        
+								</form>
+							</div>
         <?php
         // put your code here
         ?>
