897b80a39a4bc7d217bff8a2a6e06002bfd5aeb8
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-05-28 14:10:58 -0400

    metodo de crear ususarios

--- cl.automaticar.DAO/UserDAO.php
+++ cl.automaticar.DAO/UserDAO.php
@@ -9,16 +9,28 @@ class UserDAO{
     public function login(string $mail, string $pass){
         $u= new User(); 
         $conexion = mysqli_connect('localhost','root','','automaticar');
-        $sql ="SELECT * FROM USUARIO WHERE USUARIO_mail='$mail' and USUARIO_constraseña='$pass'";
+        $sql ="SELECT * FROM usuario WHERE USUARIO_mail='$mail' and USUARIO_constraseña='$pass'";
         $result = mysqli_query($conexion, $sql);
         $row = mysqli_fetch_object($result);
         print_r($row); exit;
-        if($row=!NULL){
+        if($row =! NULL){
             //$u= new User($registros->);
         }else{
+        
         }
         return u;
     }
-
+    public function agregarUsuario($nombre, $apellidoP, $apellidoM, $mail, $contraseña, $cuenta_cuentaId) {
+       $conexion = mysqli_connect('localhost', 'root', '', 'automaticar');
+       $exquery="SELECT * FROM usuario WHERE USUARIO_mail='$mail'";
+       $sql = "INSERT INTO usuario (USUARIO_id, USUARIO_nombre, USUARIO_apellido_p, USUARIO_apellido_m, USUARIO_mail, USUARIO_contraseña, CUENTA_CUENTA_id)values('$nombre', '$apellidoP', '$apellidoM','$mail','$contraseña','$cuenta_cuentaId');";
+       $comprobacion= mysqli_query($conexion, $exquery);
+       if ($comprobacion =! null) {
+           $result= mysqli_query($conexion, $sql);
+       }else{
+           
+       }
+       
+    }
 }
 
