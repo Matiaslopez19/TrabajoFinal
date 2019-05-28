1ebef568c8d9b9ea42e8a77a07063d10762a3242
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-28 16:56:50 -0400

    crear cuenta

--- cl.automaticar.view/CrearCuenta.php
+++ cl.automaticar.view/CrearCuenta.php
@@ -1,61 +1,86 @@
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <title>AutomatiCar</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="../assets/css/main.css" />
+<head>
+    <meta charset="UTF-8">
+	<title>Login Page</title>
+   <!--Made with love by Mutiullah Samim -->
+   <link href="../assets/css/cssLogin.css" rel="stylesheet" type="text/css"/>
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-        <title></title>
-    </head>
-    <body>
-        <div class="content">
-
-                                                                    
-                                                                    <h2 class="align-center">Crear cuenta</h2>
-								<hr />
-								<form action="#" method="post">
-									<div class="field">
-                                                                            <div class="field half">
-										<label>Nombre</label>
-										<input name="Nombre" id="nombre" type="text" placeholder="Nombre">
-                                                                            </div>
-                                                                            <div class="field half">
-										<label>Apellido Paterno</label>
-										<input name="apellidoP" id="apellidoP" type="text" placeholder="Apellido Paterno">
-                                                                            </div>
-                                                                            <div class="field half">
-										<label>Apellido Materno</label>
-										<input name="apellidoM" id="apellidoM" type="text" placeholder="Apellido Materno">
-                                                                            </div>
-                                                                            <div class="field half">
-										<label>Email</label>
-                                                                                <input name="email" id="email" type="email" placeholder="Email">
-                                                                            </div>
-                                                                            <div class="field half">
-										<label>Contraseña</label>
-                                                                                <input name="contraseña" id="contraseña" type="password" placeholder="Contraseña">
-                                                                            </div>
-                                                                            <div class="field half">
-										<label>Confirmar contraseña</label>
-                                                                                <input name="contraseña1" id="contraseña1" type="password" placeholder="Confirmar contraseña">
-                                                                            </div>									
-                                                                        <ul class="actions align-center">
-										<li><input type="button" class="button special" value="Atrás" onClick=" window.location.href='Login.php' "></li> 
-									</ul>
-									<ul class="actions align-center">
-										<li><input value="Crear cuenta" class="button special" type="submit"></li>
-									</ul>
-                                                                        
-								</form>
-							</div>
-        <?php
-        // put your code here
-        ?>
-    </body>
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body background="../images/544750.jpg">
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Iniciar sesión</h3>
+			</div>
+			<div class="card-body">
+				<form action="#" method="post">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+                                            <input type="text" class="form-control" name="nombre" placeholder="Nombre">
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="text" class="form-control" name="apellidoP" placeholder="Apellido paterno">
+					</div>
+                                        <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="text" class="form-control" name="apellidoM" placeholder="Apellido materno">
+					</div>
+                                        <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+                                                <input type="email" class="form-control" name="email" placeholder="Email">
+					</div>
+                                        <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+                                            <input type="password" class="form-control" name="userpass" placeholder="Contraseña">
+					</div>
+                                        <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+                                            <input type="password" class="form-control" name="userpass" placeholder="Repetir contraseña">
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Ingresar" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+                                    No tienes cuenta?<a href="CrearCuenta.php">Registrate</a>
+				</div>
+				<div class="d-flex justify-content-center">
+                                    <a href="RecuperarContrasenia.php">Olvidaste tu contraseña?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
 </html>
