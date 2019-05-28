fe482fa3abe844249c9ec9965449c3c0bfb65ba2
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-28 13:56:48 -0400

    index2

--- cl.automaticar.view/index.php
+++ cl.automaticar.view/index.php
@@ -31,7 +31,7 @@
 					<h1>AutomatiCar</h1>
 					<p>Nuestro software te brindará el control y el manejo indispensable<br/>
 					que necesitas para tu<a href="#"> Taller mecánico 2</a></p>
-					<a href="#one" class="button special scrolly">Iniciar sesión/Regístrate</a>
+					<a href="Login.php" class="button special scrolly">Iniciar sesión/Regístrate</a>
 				</div>
 			</section>
 
@@ -58,37 +58,7 @@
 
 
 			<section id="two" class="wrapper style3">
-				<div class="inner">
-					<div id="flexgrid">
-						<div>
-							<header>
-								<h3>h3 en Linea 74 de index.html</h3>
-							</header>
-							<p>Parrafo en Linea 76 de index.html</p>
-							<ul class="actions">
-								<li><a href="#" class="button alt">Learn More</a></li>
-							</ul>
-						</div>
-						<div>
-							<header>
-								<h3>h3 en Linea 83 de index.html</h3>
-							</header>
-							<p>Parrafo en Linea 85 de index.html </p>
-							<ul class="actions">
-								<li><a href="#" class="button alt">Learn More</a></li>
-							</ul>
-						</div>
-						<div>
-							<header>
-								<h3>h3 en Linea 92 de index.html</h3>
-							</header>
-							<p>Parrafo en Linea 94 de index.html</p>
-							<ul class="actions">
-								<li><a href="#" class="button alt">Learn More</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
+
 			</section>
 
 
@@ -137,58 +107,13 @@
 				<div class="inner">
 
 					<header class="align-center">
-						<h2>h2 en Linea 142 de index.html</h2>
-						<p>Parrafo en Linea 143 de index.html</p>
 					</header>
 
 				</div>
 			</section>
 
 
-			<footer id="footer" class="wrapper">
-				<div class="inner">
-					<section>
-						<div class="box">
-							<div class="content">
-								<h2 class="align-center">Agendar Hora</h2>
-								<hr />
-								<form action="#" method="post">
-									<div class="field half first">
-										<label for="name">Número de patente</label>
-										<input name="name" id="name" type="text" placeholder="Patente">
-									</div>
-									<div class="field half">
-										<label for="email">E-mail</label>
-										<input name="email" id="email" type="email" placeholder="Email">
-									</div>
-									<div class="field">
-										<label for="dept">Department</label>
-										<div class="select-wrapper">
-											<select name="dept" id="dept">
-												<option value="">- linea 170 -</option>
-												<option value="1">linea 171</option>
-												<option value="1">linea 172</option>
-												<option value="1">linea 173</option>
-												<option value="1">linea 174</option>
-											</select>
-										</div>
-									</div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
-									</div>
-									<ul class="actions align-center">
-										<li><input value="Send Message" class="button special" type="submit"></li>
-									</ul>
-								</form>
-							</div>
-						</div>
-					</section>
-					<div class="copyright">
-						&copy; linea 190 de index: <a href="https://templated.co/">TEMPLATED</a>. Images <a href="https://unsplash.com/">Unsplash</a>. Video <a href="http://coverr.co/">Coverr</a>.
-					</div>
-				</div>
-			</footer>
+			
 
 
 			<script src="../assets/js/jquery.min.js"></script>
