886777414445f9951e40a4dbdf9252a35f199081
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-05-28 14:59:42 -0400

    sxsx

--- cl.automaticar.view/index.php
+++ cl.automaticar.view/index.php
@@ -31,7 +31,7 @@
 					<h1>AutomatiCar</h1>
 					<p>Nuestro software te brindará el control y el manejo indispensable<br/>
 					que necesitas para tu<a href="#"> Taller mecánico </a></p>
-					<a href="Login.php" class="button special scrolly">Iniciar sesión/Regístrate</a>
+					<a href="Login2.php" class="button special scrolly">Iniciar sesión/Regístrate</a>
 				</div>
 			</section>
 
