1bf8245a751767b858ce769b24fc4a39f0b3735f
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-05-29 19:48:53 -0400

    revsando

--- cl.automaticar.DAO/UserDAO.php
+++ cl.automaticar.DAO/UserDAO.php
@@ -13,7 +13,7 @@ class UserDAO{
         $result = mysqli_query($conexion, $sql);
         $row = mysqli_fetch_object($result);
         if($row =! NULL){
-            $u = new User($row->USUARIO_id , $row->USUARIO_nombre, $row->USUARIO_apellido_m, $row->USUARIO_apellido_m, $row->USUARIO_mail, $row->USUARIO_clave, $row->CUENTA_CUENTA_id);            
+            $u = new User($row["USUARIO_id"] , $row["USUARIO_nombre"], $row["USUARIO_apellido_p"], $row["USUARIO_apellido_m"], $row["USUARIO_amil"], $row["USUARIO_clave"], $row["CUENTA_CUENTA_id"]);            
         }else{
         }
         return u;
