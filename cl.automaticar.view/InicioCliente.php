a6fe0da0e0bb4040194eaed9edbdf96d0409dd79
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-05-29 11:36:09 -0400

    seguir

--- cl.automaticar.view/InicioCliente.php
+++ cl.automaticar.view/InicioCliente.php
@@ -5,6 +5,7 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    
     <head>
         <title>AutomatiCar</title>
 		<meta charset="utf-8" />
