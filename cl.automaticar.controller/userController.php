542ab092ba06788db05f151babb410c70ae4e5b3
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-05-30 15:21:22 -0400

    ajasd

--- cl.automaticar.controller/userController.php
+++ cl.automaticar.controller/userController.php
@@ -1,26 +1,25 @@
 <?php
+
 session_start();
 include '../cl.automaticar.DAO/UserDAO.php';
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$userName = $_POST['username'];
-$userPass = $_POST['userpass'];
-//echo 'recibidos';
-if (empty($userName) || empty($userPass)) {
-    header("Location: http://localhost:81/TrabajoFinal/index.php");   
-}else{
-    //echo 'vamos';
-$userDAO = new UserDAO();
-$us = $userDAO->login($userName, $userPass);
-if ($us != NULL) {
-    //echo 'funciona';
-    include('../cl.automaticar.view/InicioCliente.php');
-}else{
-   $error;
-   include('../cl.automaticar.view/login2.php');
-}
+
+if (!isset($_POST['username'])) {
+        header("Location: http://localhost:81/TallerInt1/index.php");
+} else {
+    $userName = $_POST['username'];
+    $userPass = $_POST['userpass'];
+    $userDAO = new UserDAO();
+    $us = $userDAO->login($userName, $userPass);
+    if ($us != NULL) {
+        include('../cl.automaticar.view/InicioCliente.php');
+    } else {
+        $error = "Usuario o contraseña incorrecta";
+        include('../cl.automaticar.view/login2.php');
+    }
 }
 ?>
