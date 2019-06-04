b7ea80b576aa4ba315be238ca7f41043d42046dc
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-06-03 23:20:58 -0400

    Horas agendadas

--- cl.automaticar.view/HorasAgendadas.php
+++ cl.automaticar.view/HorasAgendadas.php
@@ -25,9 +25,9 @@
     <div class="bg-light border-right" id="sidebar-wrapper">
       <div class="sidebar-heading">AutomatiCar</div>
       <div class="list-group list-group-flush">
-        <a href="SeleccionarServicio.html" class="list-group-item list-group-item-action bg-light">Agendar Hora</a>
-        <a href="Historial.html" class="list-group-item list-group-item-action bg-light">Descargar Historial</a>
-        <a href="HorasAgendadas.html" class="list-group-item list-group-item-action bg-light">Horas Agendadas</a>
+          <a href="SeleccionarServicio.php" class="list-group-item list-group-item-action bg-light">Agendar Hora</a>
+          <a href="Historial.php" class="list-group-item list-group-item-action bg-light">Descargar Historial</a>
+          <a href="HorasAgendadas.php class="list-group-item list-group-item-action bg-light">Horas Agendadas</a>
       </div>
     </div>
     <!-- /#sidebar-wrapper -->
