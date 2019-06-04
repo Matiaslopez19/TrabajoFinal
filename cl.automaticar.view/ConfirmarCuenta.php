e4a3c2fce7e7efa11af0785e99096606739fd57d
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-06-03 20:44:09 -0400

    Confirmar cuenta

--- cl.automaticar.view/ConfirmarCuenta.php
+++ cl.automaticar.view/ConfirmarCuenta.php
@@ -1,41 +1,55 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <title>AutomatiCar</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="../assets/css/main.css" />
+<head>
+    <meta charset="UTF-8">
+	<title>Login Page</title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+   <!--Made with love by Mutiullah Samim -->
+   <link href="../assets/css/cssLogin.css" rel="stylesheet" type="text/css"/>
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-        <title></title>
-    </head>
-    <body>
-        <div class="content">
-
-                                                                    
-                                                                    <h2 class="align-center">Confirmar cuenta</h2>
-								<hr />
-								<form action="#" method="post">
-									<div class="field">
-                                                                            <div class="field half">
-										<label>Código de confirmación</label>
-                                                                                <input name="codigo" id="codigo" type="text" placeholder="Código">
-                                                                            </div>  
-                                                                            <div class="field half">
-                                                                                <a href="">Reenviar Código</a>
-                                                                            </div>                                                                             
-									<ul class="actions align-center">
-										<li><input value="Aceptar" class="button special" type="submit"></li>
-									</ul>
-                                                                        
-								</form>
-							</div>
-        <?php
-        // put your code here
-        ?>
-    </body>
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body background="../images/544750.jpg">
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card2">
+			<div class="card-header">
+				<h3>Confirmar cuenta</h3>
+			</div>
+			<div class="card-body">
+				<form action="#" method="post">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+                                            <input type="text" class="form-control" name="codigo" placeholder="Codigo de confirmacion">
+						
+					</div>
+					<div class="input-group form-group">
+						
+						<a href="">Reenviar Código</a>
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Aceptar" class="btn float-right login_btn">
+					</div>
+                                        <div class="form-group">
+                                            <input type="button" value="Atrás" class="btn float-left login_btn" onClick=" window.location.href='CrearCuenta.php' ">
+					</div>
+				</form>
+                            
+			</div>
+			
+		</div>
+	</div>
+</div>
+</body>
 </html>
