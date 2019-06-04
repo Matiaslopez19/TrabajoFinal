704c3825a98cb1b9d15ea45ccb0de138faf52ba0
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-04 15:09:20 -0400

    corriegiendo error

--- clAutomaticarDAO/UserDAO.php
+++ clAutomaticarDAO/UserDAO.php
@@ -36,13 +36,13 @@ class UserDAO{
            $resultado = mysqli_query($conexion, $query);
            $ultimoIsertado = mysqli_fetch_object($resultado);
            if (!is_null($result)) {
-               $u = new User($ultimoIsertado->id, $row[$nombre], 
-                    $row[$apellidoP], $row[$apellidoM], 
-                    $row[$mail], $row[$contraseña],
-                    $row[$cuenta_cuentaId]);
-               echo "esto es: ".var_dump($result);
+               $u = new User($ultimoIsertado->id, $nombre, 
+                    $apellidoP, $apellidoM, 
+                $mail, $contraseña,
+                    $cuenta_cuentaId);
+               echo "esto es: ".var_dump($u);
            }else{
-               echo "problemas: ".var_dump($result);
+               echo "problemas: ".var_dump($u);
            }
        }else{
         echo "el usuario existe o faltan datos";
