d90c099b4b4d7bf2ee5670282304439ca93563f0
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-06-04 00:07:01 -0400

    total

--- cl.automaticar.view/Historial.php
+++ cl.automaticar.view/Historial.php
@@ -3,7 +3,7 @@
 
 <head>
 
-  <link rel="stylesheet" href="../assets/css/main.css" />
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
@@ -46,7 +46,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="InicioCliente.php">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Salir</a>
