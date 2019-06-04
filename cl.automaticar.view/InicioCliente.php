ac70e8e145b472218ca173ae4f0c64099921d5d1
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-04 10:44:15 -0400

    corriegiendo error

--- cl.automaticar.view/InicioCliente.php
+++ cl.automaticar.view/InicioCliente.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
-  <title>Inicio AutomatiCar </title>
-    
-
+        <title><?php
+        echo "Bienvenido ".$us->getUSUARIO_nombre();
+        ?>
+        </title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
  <!-- Bootstrap core CSS -->
   <link href="../assets/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
   <!-- Custom styles for this template -->
