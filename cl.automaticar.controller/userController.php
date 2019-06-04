09051ef31bfef6db3fb21db1caffd5fa2e0ce45f
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-04 10:44:03 -0400

    corriegiendo error

--- cl.automaticar.controller/userController.php
+++ cl.automaticar.controller/userController.php
@@ -7,19 +7,42 @@ include '../cl.automaticar.DAO/UserDAO.php';
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-if (!isset($_POST['username'])) {
+if (isset($_POST['2'])) {
+    if (!isset($_POST['username'])) {
         header("Location: http://localhost:81/TrabajoFinal/cl.automaticar.view/index.php");
-} else {
-    $userName = $_POST['username'];
-    $userPass = $_POST['userpass'];
-    $userDAO = new UserDAO();
-    $us = $userDAO->login($userName, $userPass);
-    if ($us != NULL) {
-        include('../cl.automaticar.view/InicioCliente.php');
     } else {
-        $error = "Usuario o contraseña incorrecta";
-        include('../cl.automaticar.view/login2.php');
+        $userName = $_POST['username'];
+        $userPass = $_POST['userpass'];
+        $userDAO = new UserDAO();
+        $us = $userDAO->login($userName, $userPass);
+        if ($us != NULL) {
+            include('../cl.automaticar.view/InicioCliente.php');
+        } else {
+            $error = "Usuario o contraseña incorrecta";
+            include('../cl.automaticar.view/login2.php');
+        }
     }
-}
+} else if (isset($_POST['1'])) {
+        $nombre = $_POST['nombre'];
+        $apellidoP = $_POST['apellidoP'];
+        $apellidoM = $_POST['apellidoM'];
+        $email = $_POST['email'];
+        $userpass1 = $_POST['userpass1'];
+        $userpass2 = $_POST['userpass2'];
+        $userAccountType = 3;
+        echo 'datos recibidos bien';
+        if ($userpass1 == $userpass2) {
+            $userDAO2 = new UserDAO();
+            $us = $userDAO2->agregarUsuario($nombre, $apellidoP, $apellidoM, $email, $userpass1, $userAccountType);
+            if ($us != NULL) {
+                include ('../cl.automaticar.view/InicioCliente.php');
+            } else {
+                $errorRegistro = "error en los datos";
+                include ('../cl.automaticar.view/crearCuenta.php');
+            }
+        } else {
+            $errorClave = "las contraseñas no coinciden";
+        }
+    }
+
 ?>
