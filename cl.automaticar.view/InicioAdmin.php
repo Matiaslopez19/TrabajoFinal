7f22bcc915b6cadd231b9b65fb392d184ddae8c1
Matías Lopez <matias.lopez19@inacapmail.cl>
2019-06-03 23:51:11 -0400

    InicioADmin

--- cl.automaticar.view/InicioAdmin.php
+++ cl.automaticar.view/InicioAdmin.php
@@ -1,50 +1,83 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>AutomatiCar</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="../assets/css/main.css" />
+<html lang="en">
 
-        <title></title>
-    </head>
-    <body>
-        <div class="content">
+<head>
 
-                                                                    
-                                                                    <h2 class="align-center">Home Administrador</h2>
-								<hr />
-								<form action="#" method="post">
-									
-                                                                    <br>
-                                                                        <div class="field half">
-                                                                        <ul class="actions align-center">
-										<li><input type="button" class="button special" value="Ver horas reservadas" onClick=" window.location.href='.php' "></li> 
-									</ul>
-                                                                        </div>
-                                                                        <div class="field half">
-                                                                        <ul class="actions align-center">
-										<li><input type="button" class="button special" value="Cerrar sesión" onClick=" window.location.href='Login.php' "></li> 
-									</ul>
-                                                                        </div>
-                                                                        <div class="field half">
-									<ul class="actions align-center">
-										<li><input value="Subir PDF" class="button special" type="submit"></li>
-									</ul>
-                                                                        </div>    
-                                                                        <div class="field half">
-                                                                                    <input name="Dirección" id="direccion" type="file" placeholder="Dirección">
-									</div>
-                                                                        
-								</form>
-							</div>
-        <?php
-        // put your code here
-        ?>
-    </body>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Inicio AutomatiCar</title>
+
+
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+  <!-- Custom styles for this template -->
+  <link href="../assets/css/simple-sidebar.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">AutomatiCar</div>
+      <div class="list-group list-group-flush">
+        <a href="SeleccionarServicio.html" class="list-group-item list-group-item-action bg-light">Horas Agendadas</a>
+        <a href="Historial.html" class="list-group-item list-group-item-action bg-light">Subir Historial  </a>
+        <a href="HorasAgendadas.html" class="list-group-item list-group-item-action bg-light">Horas Agendadas</a>
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Salir</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid">
+        <h1 class="mt-4">Servicios</h1>
+        <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="../assets/css/vendor/jquery/jquery.min.js" type="text/javascript"></script>
+  <script src="../assets/css/vendor/bootstrap/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+
+  <!-- Menu Toggle Script -->
+  <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script>
+
+</body>
+
 </html>
