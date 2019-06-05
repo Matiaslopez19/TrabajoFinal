74ca67825e5ba393080051fc9060483aa7efbf3a
Dylann34 <50646135+Dylann34@users.noreply.github.com>
2019-06-04 23:05:03 -0400

    añadí cambios a inicio cliente, arreglé los href

--- clAutomaticarController/agendaController.php
+++ clAutomaticarController/agendaController.php
@@ -3,8 +3,8 @@
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * and open the template in the editor. cambié weás
+ */ 
 $servicio = $_POST['servicio'];
 $opcion = $_POST['opcion'];
 
