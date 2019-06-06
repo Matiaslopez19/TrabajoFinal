6a4754b8a48d8f75832a26731a64052a58bc7e2e
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-06 14:47:30 -0400

    creacion de controlador de agendamiento

--- clAutomaticarDAO/UserDAO.php
+++ clAutomaticarDAO/UserDAO.php
@@ -11,16 +11,18 @@ class UserDAO{
         $conexion = mysqli_connect('localhost','root','','automaticar');
         $sql ="SELECT * FROM usuario WHERE USUARIO_mail='$mail' and USUARIO_clave='$pass';";
         $result = mysqli_query($conexion, $sql);
-        if(!$result){
-            echo "no hubo resultados";
-            var_dump($result);
-        }else{
-            var_dump($result);
+        
+        if($result->num_rows == 1){
+             var_dump($result);
             $row = mysqli_fetch_array($result);
             $u = new User($row["USUARIO_id"], $row["USUARIO_nombre"], 
                     $row["USUARIO_apellido_p"], $row["USUARIO_apellido_m"], 
                     $row["USUARIO_mail"], $row["USUARIO_clave"],
                     $row["CUENTA_CUENTA_id"]);
+            
+        }else{
+           echo "no hubo resultados";
+            var_dump($result);
         }
         return $u;
         }
