65e4fdf7a3da97a6ebc8240d3ef02b6ede69d3c8
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-06 18:59:41 -0400

    creacion de controlador de agendamiento

--- clAutomaticarController/serviciosController.php
+++ clAutomaticarController/serviciosController.php
@@ -1,15 +1,6 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "automaticar") or
-                die("Problemas con la conexión");
-// esto sería para mostrar las horas agendadas
-$servicios = mysqli_query($conexion, "select AGENDAR_hora,AGENDAR_fecha,AGENDAR_tipo_servicio,AGENDAR_patente,AGENDAR_direccion"
-        . ",AGENDAR_ruta_foto,AGENDAR_region from agendar") or
-                die("Problemas en el select:" . mysqli_error($conexion));
-$user = $_SESSION['user'] ;
-        while ($reg = mysqli_fetch_array($servicios)) {
-            echo "Hora: " . $reg['AGENDAR_hora'] . "<br>";
-            echo "Fecha: " . $reg['AGENDAR_fecha'] . "<br>";
-            echo "Servicio: " . $reg['AGENDAR_tipo_servicio'] . "<br>";
-            echo "Patente: " . $reg['AGENDAR_patente'] . "<hr>";
-            echo "Dirección: " . $reg['AGENDAR_direccion'] . "<hr>";
-        }
+     session_start();
+     include '../clAutomaticarData/Servicios';
+     if (!$_SESSION["user"]) {
+    
+}
