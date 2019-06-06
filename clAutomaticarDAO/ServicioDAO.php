a33830a76992fbda68f84e7f814631e57bd54a36
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-06 16:06:25 -0400

    creacion de controlador de agendamiento

--- clAutomaticarDAO/ServicioDAO.php
+++ clAutomaticarDAO/ServicioDAO.php
@@ -5,4 +5,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+include '../clAutomaticarData/Servicio.php';
+include '../clAutomaticarData/conexion.php';
 
+class ServicioDAO{
+    public static function findAllMantencion() {
+        $conexion= new Conexion();
+        $se=null;
+        $sql="SELECT * FROM servicios where se_tipo= 1";
+        $result= mysqli_query($conexion->conn(), $sql);
+        if ($result->num_rows >=1) {
+            $row= mysqli_fetch_array($result);
+            $se= new Servicio($row[se_id], $row["se_nombre"], $row["se_tipo"], $row["se_estado"]);
+        }
+        return $se;
+    }
+}
