1d47c7cab6d4d5e19ebea30938b0faf873924a2a
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-06 23:04:17 -0400

    motodo para mostrar servicios reparacion

--- clAutomaticarController/serviciosController.php
+++ clAutomaticarController/serviciosController.php
@@ -1,11 +1,18 @@
 <?php
-     session_start();
-     include '../clAutomaticarData/Servicio.php';
-     include '../clAutomaticarDAO/ServicioDAO.php';
+     //session_start();
+     //include '../clAutomaticarData/Servicio.php';
+     //include '../clAutomaticarDAO/ServicioDAO.php';
      /*if (!$_SESSION["user"]) {
     
 }*/
 //echo $id;
 if (isset($_GET['ser'])) {
     $id=$_GET['ser'];
+    if (isset($_SESSION['user'])) {
+        include '../clAutomaticarView/SeleccionarServicio.php#one';
+    } else {
+        include '../clAutomaticarView/Login.php';
+    }
+}else{
+    echo "ne necesita seleccionar un servicio primero";
 }
