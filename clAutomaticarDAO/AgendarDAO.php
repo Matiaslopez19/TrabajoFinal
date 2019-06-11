b7bb883164ce76f88c20a55c2bf8cc4f57ba207d
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-11 12:02:39 -0400

    cambios getter setter y constructor

--- clAutomaticarDAO/AgendarDAO.php
+++ clAutomaticarDAO/AgendarDAO.php
@@ -1,5 +1,5 @@
 <?php
-
+include '../clAutomaticarData/conexion.php';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,7 +7,11 @@
  */
 
 class AgendarDAO{
-    public function AgregarAgenda($param) {
-        
+    public function AgregarAgenda($lugar, $fecha, $servicioId, $clienteId) {
+        $conexion = Conexion::conn();
+        $sql="INSERT INTO agenda(ag_id, ag_estado, ag_lugar, ag_fecha, Servicio_ser_id, "
+                . "Cliente_cli_id, Disponibilidad_disp_id) VALUES "
+                . "([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])";
+        $sqlComprobacion;
     }
 }
