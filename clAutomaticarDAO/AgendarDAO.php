9f8301c4db3e5b362d42dac0f60115659bfd9524
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-06-13 15:13:02 -0400

    cookies agregadas y full calendar implementado (en proceso), destruccion de session establecido

--- clAutomaticarDAO/AgendarDAO.php
+++ clAutomaticarDAO/AgendarDAO.php
@@ -14,4 +14,7 @@ class AgendarDAO{
                 . "([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])";
         $sqlComprobacion;
     }
+    public static function FindAllAgenda(){
+        
+    }
 }
