8bc30c083e7f0bcdd1b9afb5c23238d6a54735e1
SEBASTIAN ANDRES SUAZO QUINTEROS <sebastian.suazo05@inacapmail.cl>
2019-07-04 14:28:51 -0400

    zona horaria corregida(apenas)

--- clAutomaticarController/userController.php
+++ clAutomaticarController/userController.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include '../clAutomaticarDAO/UserDAO.php';
 /*
@@ -8,43 +9,52 @@ include '../clAutomaticarDAO/UserDAO.php';
  */
 if (isset($_POST['2'])) { //se revisa el name del boton del formulario
     if (!isset($_POST['username'])) { //se verifica si el campo de nombre esta lleno o vacio
-        include('../clAutomaticarView/Login.php');// si no esta lleno, te redireccionara al index
+        include('../clAutomaticarView/Login.php'); // si no esta lleno, te redireccionara al index
     } else {
-        $userName = $_POST['username'];//toma el mail
-        $userPass = $_POST['userpass'];//toma la clave
-        $userDAO = new UserDAO();//llama a la clase DAO
-        $us = $userDAO->login($userName, $userPass);
-        //var_dump($us);
-        if ($us) {
-            $_SESSION['user'] = $us;
-            include('../clAutomaticarView/SeleccionarServicio.php');
+        $userName = $_POST['username']; //toma el mail
+        $userPass = $_POST['userpass']; //toma la clave
+        $userDAO = new UserDAO(); //llama a la clase DAO
+        if ($userName == "sebalokoshoon98@gmail.com") {
+            $us = $userDAO->LoginAdmin($userName, $userPass);
+            if (!is_null($us)) {
+                $_SESSION['administrador'] = $u;
+                include('../clAutomaticarView/InicioAdmin.php');
+            }else{
+                include '../clAutomaticarView/Login.php';
+            }
         } else {
-            include('../clAutomaticarView/Login.php');
-        }
-        
-    }
-} else if (isset($_POST['1'])) {
-        $nombre = $_POST['nombre'];
-        $apellidoP = $_POST['apellidoP'];
-        $apellidoM = $_POST['apellidoM'];
-        $email = $_POST['email'];
-        $userpass1 = $_POST['userpass1'];
-        $userpass2 = $_POST['userpass2'];
-        $userAccountType = 3;
-        if ($userpass1 == $userpass2) {
-            $userDAO2 = new UserDAO();
-            $us = $userDAO2->agregarUsuario($nombre, $apellidoP, $apellidoM, $email, $userpass1, $userAccountType);
-            if ($us != NULL) {
-                $_SESSION['user']=$us;
-                include ('../clAutomaticarView/InicioCliente.php');
+            $us = $userDAO->login($userName, $userPass);
+            //var_dump($us);
+            if ($us) {
+                $_SESSION['user'] = $us;
+                include('../clAutomaticarView/SeleccionarServicio.php');
             } else {
-                $errorRegistro = "error en los datos";
-                include ('../clAutomaticarView/crearCuenta.php');
+                include('../clAutomaticarView/Login.php');
             }
         }
     }
-    if (isset($_GET['salir'])) {
-        session_destroy();
-        include ('../clAutomaticarView/Login.php');
+} else if (isset($_POST['1'])) {
+    $nombre = $_POST['nombre'];
+    $apellidoP = $_POST['apellidoP'];
+    $apellidoM = $_POST['apellidoM'];
+    $email = $_POST['email'];
+    $userpass1 = $_POST['userpass1'];
+    $userpass2 = $_POST['userpass2'];
+    $userAccountType = 3;
+    if ($userpass1 == $userpass2) {
+        $userDAO2 = new UserDAO();
+        $us = $userDAO2->agregarUsuario($nombre, $apellidoP, $apellidoM, $email, $userpass1, $userAccountType);
+        if ($us != NULL) {
+            $_SESSION['user'] = $us;
+            include ('../clAutomaticarView/InicioCliente.php');
+        } else {
+            $errorRegistro = "error en los datos";
+            include ('../clAutomaticarView/crearCuenta.php');
+        }
+    }
+}
+if (isset($_GET['salir'])) {
+    session_destroy();
+    include ('../clAutomaticarView/Login.php');
 }
 ?>
